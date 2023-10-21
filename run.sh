11377931d934eff313e77e86aa40a38526dad53c
jacob-corletto <jacob.corletto@csu.fullerton.edu>
2023-10-21 00:28:19 -0700

    changed run.sh file

--- run.sh
+++ run.sh
@@ -1,12 +1,12 @@
 rm *.o
 rm *.out
 
-g++ -c -m64 -Wall -g -o node.o node.cpp -fno-pie -std=c++2a
+g++ -c -m64 -Wall -g -o node.o node.cpp -fpie -std=c++2a
 
-g++ -c -m64 -Wall -g -o main.o main.cpp -fno-pie -std=c++2a
+g++ -c -m64 -Wall -g -o main.o main.cpp -fpie -std=c++2a
 
-g++ -c -m64 -Wall -g -o tree.o tree.cpp -fno-pie -std=c++2a
+g++ -c -m64 -Wall -g -o tree.o tree.cpp -fpie -std=c++2a
 
-g++ -m64 -g -o run.out  main.o node.o tree.o -fno-pie -std=c++2a
+g++ -m64 -g -o run.out  main.o node.o tree.o -fpie -std=c++2a
 
 ./run.out
