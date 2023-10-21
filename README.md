d8314f4801bab5f4b30920527ce0d58e2ba96b9f
jacob-corletto <jacob.corletto@csu.fullerton.edu>
2023-10-21 00:20:13 -0700

    cleaning up files changed run.sh -std=c++2a

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Binary Tree Database
-    This program is used as a subdirectory to store information in a Binary tree.
+This program is used as a subdirectory to store information in a Binary tree.
+
+WORKS:
+inseartion 
+printing inorder
+search
+node class (getter and setter functions)
+Tree class 
--- run.sh
+++ run.sh
@@ -1,12 +1,12 @@
 rm *.o
 rm *.out
 
-g++ -c -m64 -Wall -g -o node.o node.cpp -fno-pie -std=c++20
+g++ -c -m64 -Wall -g -o node.o node.cpp -fno-pie -std=c++2a
 
-g++ -c -m64 -Wall -g -o main.o main.cpp -fno-pie -std=c++20
+g++ -c -m64 -Wall -g -o main.o main.cpp -fno-pie -std=c++2a
 
-g++ -c -m64 -Wall -g -o tree.o tree.cpp -fno-pie -std=c++20
+g++ -c -m64 -Wall -g -o tree.o tree.cpp -fno-pie -std=c++2a
 
-g++ -m64 -g -o run.out  main.o node.o tree.o -fno-pie -std=c++20
+g++ -m64 -g -o run.out  main.o node.o tree.o -fno-pie -std=c++2a
 
 ./run.out
